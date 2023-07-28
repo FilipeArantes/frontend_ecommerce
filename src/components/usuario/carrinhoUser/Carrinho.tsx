f57703e3a47a48b1cf0d65fbde8cb4f130268fbd
Filipe Arantes <filharantes@gmail.com>
2023-07-28 00:04:03 -0300

    atualização final

--- src/components/usuario/carrinhoUser/Carrinho.tsx
+++ src/components/usuario/carrinhoUser/Carrinho.tsx
@@ -1,21 +1,24 @@
-import { useState } from "react";
 import InformacoesModal from "./InformacoesModal";
 import ItensModalIndividual from "./ItensModalIndividual";
+import IpeClose from "@/src/assets/icones/ipe-close.svg";
+import Image from "next/image";
 
 type CarrinhoProps = {
   fechar: () => void;
 };
 
-export default function Carrinho({
-  fechar,
-}: CarrinhoProps) {
+export default function Carrinho({ fechar }: CarrinhoProps) {
   return (
     <div>
-      <span onClick={fechar}>X</span>
-      <div className="flex flex-row justify-start gap-16 pl-6">
+      <div className="flex justify-end">
+        <span className="" onClick={fechar}>
+          <Image src={IpeClose} alt="" />
+        </span>
+      </div>
+      <div className="flex flex-row justify-start gap-16 ">
         <ItensModalIndividual />
         <div>
-          <InformacoesModal fechar={fechar}/>
+          <InformacoesModal fechar={fechar} />
         </div>
       </div>
     </div>
